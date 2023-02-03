0e9f6337f7145a9977498f8d2a4caaa98e0d09b6
andry.anthony <astro@node.com>
2023-02-03 13:56:31 +0300

    Intial commit

---
+++ src/main/java/com/eqima/eqimaid/EqimaidApplication.java
@@ -0,0 +1,13 @@
+package com.eqima.eqimaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EqimaidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EqimaidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eqima/eqimaid/EqimaidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eqima.eqimaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EqimaidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
